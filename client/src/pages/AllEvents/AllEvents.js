deee72e7b73ae43c30eb16c35514b0e33f6fd2cb
Michael Dillie <michaeldillie@gmail.com>
2018-07-04 15:02:07 -0500

    Stopping point

--- client/src/pages/AllEvents/AllEvents.js
+++ client/src/pages/AllEvents/AllEvents.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./AllEvents.css";
 import API from "../../utils/API";
+import { Col, Row, Container } from "../../components/Grid";
+import EventCard from "../../components/EventCard";
 
 class AllEvents extends Component {
 
@@ -31,37 +33,88 @@ class AllEvents extends Component {
       )
   }
 
+  userMatch = () => {
+    // console.log("triggered");
+    this.state.events.map( test => {
+      // console.log(test.userId);
+      if(test.userId === this.state.profile.sub) {
+        // console.log("true");
+        return true
+      } else {
+        // console.log("false");
+        return false
+      }
+    })
+  }
+
   render() {
     return(
-      <div>
-        <h1>Hello World</h1>
+      <Container>
+        <div>
+          < EventCard 
+            userMatch={this.userMatch}
+          />
+        </div>
         {
           this.state.events.map( singleEvent => {
             if(singleEvent.userId === this.state.profile.sub) {
               return (
-                <div key="singleEvent._id">
-                  <p>{singleEvent.eventName}</p>
-                  <p>{singleEvent.date}</p>
-                  <p>{singleEvent.location}</p>
-                  <p>{singleEvent.description}</p>
-                  <button>EDIT</button>
-                  <button>DELETE</button>
-                </div>
+                <Row key={singleEvent._id}>
+                  <Col size="12">
+                    <Row>
+                      <Col size="6">
+                        <p>{singleEvent.eventName}</p>
+                        <p>{singleEvent.location}</p>
+                      </Col>
+                      <Col size="6">
+                        <p>{singleEvent.date}</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col size="3"></Col>
+                      <Col size="6">
+                        <p>{singleEvent.description}</p>
+                      </Col>
+                      <Col size="3"></Col>
+                    </Row>
+                    {/* <p>{singleEvent.eventName}</p>
+                    <p>{singleEvent.date}</p>
+                    <p>{singleEvent.location}</p>
+                    <p>{singleEvent.description}</p> */}
+                    {/* EDIT AND DELETE MIGHT BE REMOVED AND PLACED IN THE USERS PROFILE */}
+                    <button>EDIT</button>
+                    <button>DELETE</button>
+                  </Col>
+                </Row>
               )
             } else {
               return (
-                <div key="singleEvent._id">
-                  <p>{singleEvent.eventName}</p>
-                  <p>{singleEvent.date}</p>
-                  <p>{singleEvent.location}</p>
-                  <p>{singleEvent.description}</p>
-                  <button>SPONSOR</button>
-                </div>
+                <Row key={singleEvent._id}>
+                  <Col size="12">
+                    <Row>
+                      <Col size="6">
+                        <p>{singleEvent.eventName}</p>
+                        <p>{singleEvent.location}</p>
+                      </Col>
+                      <Col size="6">
+                        <p>{singleEvent.date}</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col size="3"></Col>
+                      <Col size="6">
+                        <p>{singleEvent.description}</p>
+                      </Col>
+                      <Col size="3"></Col>
+                    </Row>
+                    <button>SPONSOR</button>
+                  </Col>
+                </Row>
               )
             }
           })
         }
-      </div>
+      </Container>
     )
   }
 }
